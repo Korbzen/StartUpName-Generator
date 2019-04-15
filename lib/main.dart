b407897b4ffd4a1ced968d7bd237e2c4ba17c0a0
Korbinian <Korbi.Hitthaler@live.de>
2019-04-15 22:00:03 +0200

    Added the EnglishWords package to create random Wordpairings Added a Stateful widget to be able to extend the view during runtime

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
       theme: new ThemeData(
         primarySwatch: Colors.green,
       ),
@@ -15,10 +16,24 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text('Hello World'),
+            child: RandomWords(),
         ),
       ),
     );
   }
 }
 
+// this class creates its State class
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+// create a stateful widget to be able to change content
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
