c7fb1dc190ae44e265bcc8235da28d33952a6ef9
Korbinian <Korbi.Hitthaler@live.de>
2019-04-15 23:47:39 +0200

    Tried to add an app icon and did some styling to the app using the material cards

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
-      theme: new ThemeData(
-        primarySwatch: Colors.green,
+      theme: ThemeData(
+        primaryColor: Colors.white,
       ),
       home: RandomWords(),
       );
   }
 }
+
 
 // this class creates its State class
 class RandomWords extends StatefulWidget {
@@ -22,21 +23,21 @@ class RandomWords extends StatefulWidget {
   RandomWordsState createState() => new RandomWordsState();
 }
 
+
 // create a stateful widget to be able to change content
 class RandomWordsState extends State<RandomWords> {
   @override
   final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>(); // This saves the state of the Name
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
-  // This builds an infinite list
+    // This builds an infinite list
     Widget _buildSuggestions() {
       return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(6.0),
         itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider();
-          /*2*/
+          final index = i; /*3*/
 
-          final index = i ~/ 2; /*3*/
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10)); /*4*/
           }
@@ -45,20 +46,77 @@ class RandomWordsState extends State<RandomWords> {
     }
 
     Widget _buildRow(WordPair pair) {
-      return ListTile(
-        title: Text(
-          pair.asPascalCase,
-          style: _biggerFont,
+      final bool alreadySaved = _saved.contains(pair);
+      return Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+              trailing: Icon(   // Add the lines from here...
+                alreadySaved ? Icons.star : Icons.star_border,
+                color: alreadySaved ? Colors.yellow : null,
+              ),
+              onTap: () {      // Interactive event
+                setState(() {
+                  if (alreadySaved) {
+                    _saved.remove(pair);
+                  } else {
+                    _saved.add(pair);
+                  }
+                });
+              },
+            ),
+          ]
         ),
       );
     }
 
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
+    void _pushSaved() {
+      Navigator.of(context).push(
+        MaterialPageRoute<void>(
+          builder: (BuildContext context) {
+            final Iterable<ListTile> tiles = _saved.map(
+                  (WordPair pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+            );
+            final List<Widget> divided = ListTile
+                .divideTiles(
+              context: context,
+              tiles: tiles,
+            )
+                .toList();
+
+            // Creates a new Scaffold/Window
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Saved Suggestions'),
+              ),
+              body: ListView(children: divided),
+            );
+          },
+
+        ),
+      );
+    }
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.star), onPressed: _pushSaved),
+          ],
+          title: Text('Startup Name Generator'),
+
+        ),
+        body: _buildSuggestions(),
+      );
+    }
 }
